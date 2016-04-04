314613472eb54d676a629bac7f37c0ed4bc609b9
lawtonB <lawton.a.browning@gmail.com>
2016-04-04 10:21:07 -0700

    Move destroyCategory to admin controller

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('category');
-  },
-
-  actions: {
-    destroyCategory(category) {
-      category.destroyRecord();
-    }
   }
+
 });
